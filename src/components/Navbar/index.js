c48cab18e3ec9ab591eaa8dccdb268a61043f47c
Henry Fajardo <lagosjp07@outlook.es>
2025-01-29 08:57:23 -0600

    Refactor Navbar component layout; adjust spacing and remove commented code

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -16,9 +16,9 @@ const Navbar = () => {
     <nav className="bg-white Navbar dark:bg-gray-900 fixed w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-600">
       <div className="max-w-screen-xl flex flex-wrap h-[90px] items-center justify-start mx-auto p-2 w-full">
         <NavbarLogo logo={logo} />
-        {/* <div className="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
+        <div className="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
           <NavbarMenuButton />
-        </div> */}
+        </div>
         <div
           className="flex items-center w-full h-full pt-[2px] md:flex md:w-auto md:order-1 ml-[11.55rem]"
           id="navbar-sticky"
